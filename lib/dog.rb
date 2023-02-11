b6d896e06551c379cbbb3f5ce157c11a31d1a8ab
OgembaJr <emmanuel.ogemba@student.moringaschool.com>
2023-02-11 11:42:33 +0300

    understood setter and getter methods

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+    # setter method
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+    #getter method
+    def name
+        # access the data from the @name instance variable and return it
+      @this_dogs_name
+    end
+  end
 
-end
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  puts lassie.name
